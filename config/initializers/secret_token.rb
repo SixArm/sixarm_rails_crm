4c4254662bd48e75bed0c088b7dc3239b45fe4b4
SixArm <sixarm@sixarm.com>
2011-04-24 16:17:32 -0700

    Run rails new hello --database=mysql --skip-test-unit --skip-prototype

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SixarmRailsCrm::Application.config.secret_token = 'a72a5b099ab4d43e8b62c3c9fc0281e5f4f6d6915c3b8b769a14e848956c1714a5baddee4df5128c36170cdfa5ff5561e2e85acafd24317bba31853046e7c73e'
